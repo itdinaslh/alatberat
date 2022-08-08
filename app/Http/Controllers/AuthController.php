4fdf40cdd76ad71757fd6e2c378bc58a14aa45a3
itdinaslh <itdlh@outlook.com>
2022-08-09 01:15:19 +0700

    Update Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Models\User;
+use Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string',
+            'username' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|',
+            'c_password'=>'required|same:password',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        if($user->save()){
+            return response()->json([
+                'message' => 'User sukses ditambahkan!'
+            ], 201);
+        }else{
+            return response()->json(['error'=>'Detail belum lengkap!']);
+        }
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+        ], 401);
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('Personal Access Token');
+
+        $token = $tokenResult->token;
+
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString()
+            ]);
+    }
+
+    public function user(Request $request) {
+        return response()->json($request->user());
+    }
 }
